5923c8112bfaf48fed48bd605f20e69050640566
Sam Weber <spyder255@gmail.com>
2016-01-22 09:10:34 -0600

    Adding initial solution files to repo

---
+++ Sweb.Chess.BoardHelper/ChessBoardForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Sweb.Chess.BoardHelper
+{
+    partial class ChessBoardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChessBoardForm));
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.stayOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opacityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem12 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem11 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stayOnTopToolStripMenuItem,
+            this.opacityToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(135, 92);
+            // 
+            // stayOnTopToolStripMenuItem
+            // 
+            this.stayOnTopToolStripMenuItem.Name = "stayOnTopToolStripMenuItem";
+            this.stayOnTopToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.stayOnTopToolStripMenuItem.Text = "Stay on top";
+            this.stayOnTopToolStripMenuItem.Click += new System.EventHandler(this.ContextMenuAction_Clicked);
+            // 
+            // opacityToolStripMenuItem
+            // 
+            this.opacityToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem12,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9,
+            this.toolStripMenuItem10,
+            this.toolStripMenuItem11});
+            this.opacityToolStripMenuItem.Name = "opacityToolStripMenuItem";
+            this.opacityToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.opacityToolStripMenuItem.Text = "Opacity";
+            this.opacityToolStripMenuItem.Click += new System.EventHandler(this.ContextMenuAction_Clicked);
+            // 
+            // toolStripMenuItem12
+            // 
+            this.toolStripMenuItem12.Name = "toolStripMenuItem12";
+            this.toolStripMenuItem12.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem12.Text = "0%";
+            this.toolStripMenuItem12.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem2.Text = "10%";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem3.Text = "20%";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem4.Text = "30%";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem5.Text = "40%";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem6.Text = "50%";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem7.Text = "60%";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem8.Text = "70%";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem9.Text = "80%";
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem10
+            // 
+            this.toolStripMenuItem10.Name = "toolStripMenuItem10";
+            this.toolStripMenuItem10.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem10.Text = "90%";
+            this.toolStripMenuItem10.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem11
+            // 
+            this.toolStripMenuItem11.Name = "toolStripMenuItem11";
+            this.toolStripMenuItem11.Size = new System.Drawing.Size(102, 22);
+            this.toolStripMenuItem11.Text = "100%";
+            this.toolStripMenuItem11.Click += new System.EventHandler(this.OpacityMenuItem_Clicked);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.toolStripMenuItem1.Text = "Flip Board";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.FlipBoard_Clicked);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ContextMenuAction_Clicked);
+            // 
+            // ChessBoardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(358, 358);
+            this.ContextMenuStrip = this.contextMenu;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ChessBoardForm";
+            this.Opacity = 0.5D;
+            this.Text = "ChessBoardForm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMouseUp);
+            this.Resize += new System.EventHandler(this.OnResize);
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem stayOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opacityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem11;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem12;
+    }
+}
---
+++ Sweb.Chess.BoardHelper/ChessBoardForm.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sweb.Chess.BoardHelper
+{
+    public partial class ChessBoardForm : Form
+    {
+
+        #region Window Dragging Properties and Methods
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        #endregion
+
+        private const int resizeBorderWidth = 7;
+        private bool isWhite = true;
+        private int opacity = 102;
+
+        public ChessBoardForm()
+        {
+            InitializeComponent();
+
+            // Set Window style flags. Not sure what all of these are for yet
+            this.SetStyle(ControlStyles.ResizeRedraw, true);// | ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+        }
+
+        #region Form Events
+                        
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void OnResize(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            this.DrawBoard(e.Graphics, this.Width, this.Height);
+        }
+        
+
+        /// <summary>
+        /// Used to add window resizing functionality to the form even though the border has been disabled
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            const uint WM_NCHITTEST = 0x0084;
+            const uint WM_MOUSEMOVE = 0x0200;
+
+            const uint HTLEFT = 10;
+            const uint HTRIGHT = 11;
+            const uint HTBOTTOMRIGHT = 17;
+            const uint HTBOTTOM = 15;
+            const uint HTBOTTOMLEFT = 16;
+            const uint HTTOP = 12;
+            const uint HTTOPLEFT = 13;
+            const uint HTTOPRIGHT = 14;
+
+            const int RESIZE_HANDLE_SIZE = 10;
+            bool handled = false;
+
+            if (m.Msg == WM_NCHITTEST || m.Msg == WM_MOUSEMOVE)
+            {
+                var formSize = this.Size;
+                var screenPoint = new Point(m.LParam.ToInt32());
+                var clientPoint = this.PointToClient(screenPoint);
+
+                var boxes = new Dictionary<uint, Rectangle>() {
+                    {HTBOTTOMLEFT, new Rectangle(0, formSize.Height - RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE)},
+                    {HTBOTTOM, new Rectangle(RESIZE_HANDLE_SIZE, formSize.Height - RESIZE_HANDLE_SIZE, formSize.Width - 2*RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE)},
+                    {HTBOTTOMRIGHT, new Rectangle(formSize.Width - RESIZE_HANDLE_SIZE, formSize.Height - RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE)},
+                    {HTRIGHT, new Rectangle(formSize.Width - RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, formSize.Height - 2*RESIZE_HANDLE_SIZE)},
+                    {HTTOPRIGHT, new Rectangle(formSize.Width - RESIZE_HANDLE_SIZE, 0, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE) },
+                    {HTTOP, new Rectangle(RESIZE_HANDLE_SIZE, 0, formSize.Width - 2*RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE) },
+                    {HTTOPLEFT, new Rectangle(0, 0, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE) },
+                    {HTLEFT, new Rectangle(0, RESIZE_HANDLE_SIZE, RESIZE_HANDLE_SIZE, formSize.Height - 2*RESIZE_HANDLE_SIZE) }
+                };
+
+                foreach (KeyValuePair<uint, Rectangle> hitBox in boxes)
+                {
+                    if (hitBox.Value.Contains(clientPoint))
+                    {
+                        m.Result = (IntPtr)hitBox.Key;
+                        handled = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!handled)
+                base.WndProc(ref m);
+        }
+
+        #endregion
+
+        #region Control Events
+
+        private void ContextMenuAction_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as ToolStripMenuItem;
+            if (menuItem != null)
+            {
+                switch (menuItem.Text.ToUpper())
+                {
+                    case "EXIT":
+                        this.Close();
+                        break;
+                    case "STAY ON TOP":
+                        menuItem.Checked = !menuItem.Checked;
+                        this.TopMost = menuItem.Checked;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void OpacityMenuItem_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as ToolStripMenuItem;
+            if (menuItem != null)
+            {
+                double opacityFromMenuItem;
+                if (double.TryParse(menuItem.Text.Replace("%", ""), out opacityFromMenuItem))
+                {
+                    this.opacity = Convert.ToInt32(opacityFromMenuItem / 100 * 255);
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void FlipBoard_Clicked(object sender, EventArgs e)
+        {
+            this.isWhite = !this.isWhite;
+            this.Refresh();
+        }
+
+        #endregion
+
+        private void DrawBoard(Graphics graphics, int windowWidth, int windowHeight)
+        {
+            var fileLetters = new string[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+
+            var verticalBoardPadding = windowHeight * 0.05f;
+            var horizontalBoardPadding = windowWidth * 0.05f;
+
+            var verticalFontSize = verticalBoardPadding * 0.9f;
+            var horizontalFontSize = horizontalBoardPadding * 0.9f;
+
+            var boardHeight = windowHeight - verticalBoardPadding;
+            var boardWidth = windowWidth - horizontalBoardPadding;
+
+            var squareHeight = boardHeight / 8.0f;
+            var squareWidth = boardWidth / 8.0f;
+
+            // Window Border
+            graphics.DrawRectangle(Pens.Black, 0, 0, windowWidth - 1, windowHeight - 1);
+
+            graphics.FillRectangle(new SolidBrush(Color.FromArgb(1, Color.LimeGreen)), 0, 0, windowWidth - 1, windowHeight - 1);
+
+            // Board Border
+            graphics.DrawRectangle(Pens.Black, 0, 0, boardWidth, boardHeight);
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int file = 0; file < 8; file++)
+                {
+                    if ((row % 2 == 0 && file % 2 == 1) ||
+                        (row % 2 == 1 && file % 2 == 0))
+                    {
+                        // Draw Square
+                        graphics.FillRectangle(new SolidBrush(Color.FromArgb(this.opacity, 0, 0, 0)), file * squareWidth, row * squareHeight, squareWidth, squareHeight);
+                    }
+                }
+
+                // Draw files and row labels
+                graphics.DrawString(fileLetters[this.isWhite ? row : 7 - row], new Font(FontFamily.GenericMonospace, verticalFontSize, GraphicsUnit.Pixel), Brushes.Black, row * squareWidth + (squareWidth / 2) - verticalFontSize / 2, boardHeight + verticalBoardPadding / 2 - verticalFontSize / 2 - 2);
+                graphics.DrawString($"{(this.isWhite ? (8 - row) : (row + 1))}", new Font(FontFamily.GenericMonospace, horizontalFontSize, GraphicsUnit.Pixel), Brushes.Black, boardWidth + horizontalBoardPadding / 2 - verticalFontSize / 2, row * squareHeight + squareHeight / 2 - verticalFontSize / 2);
+            }
+        }
+    }
+}
